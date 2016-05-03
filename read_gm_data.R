2c81a0726303e279b927585893f3823b6fb95711
James Faulkner <jfaulkn@umich.edu>
2016-05-03 15:45:09 -0400

    Added Comment

--- read_gm_data.R
+++ read_gm_data.R
@@ -2,6 +2,9 @@ gdp <- read.csv("data/data-1952.csv")
 new <- read.csv("data/data-1957.csv")
 gdp <- rbind(gdp, new)
 
+
+# Calculating GDP by James
+
 gdp$gdp  <- gdp$gdpPercap * gdp$pop
 
 keep <- c('country', 'year', 'lifeexp', 'gdp')
